28fb51382b1498100debc724aac7fa39f9e52034
LauraEsmoris <lesmoris0web@gmail.com>
2023-06-11 12:29:22 -0500

    tickTakToe done

---
+++ src/Components/games/tik-tak-toe/TikTakToe.tsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+
+export function TikTakToe() {
+
+   const [fields, setFilds] = useState<string>("")
+
+   const [turnA, setTurnA] = useState<boolean>(true)
+
+   const [isUsed, setIsUsed] = useState<boolean>(false)
+   function markField(){
+      if(!isUsed){
+         if (turnA){
+            setFilds("X");
+            setTurnA(false);
+            setIsUsed(true)
+         }
+         else {
+            setFilds("O");
+            setTurnA(true);
+            setIsUsed(true)
+         }
+      }
+   }
+
+   return(
+      <div className="general-container">
+         <div className="row">
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+         </div>
+         <div className="row">
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+         </div>
+         <div className="row">
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+            <div className="square" onClick={()=>{markField()}}>
+               <h2>{fields}</h2>
+            </div>
+         </div>
+      </div>
+   )
+
+}
