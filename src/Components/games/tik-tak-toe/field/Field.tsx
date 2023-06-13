5764261f099d89892a2ff6ba3b7d442462a19d7f
josedavms13 <54677583+josedavms13@users.noreply.github.com>
2023-06-13 15:23:55 -0500

    Refactor: Field moved to it's own component

---
+++ src/Components/games/tik-tak-toe/field/Field.tsx
@@ -0,0 +1,16 @@
+import {IFieldBox} from "./field.types.ts";
+import "./field.css";
+
+export function Field({fieldMark, isLocked, id, onFieldSelected}: IFieldBox) {
+   function handleFieldClick() {
+      if (!isLocked) {
+         onFieldSelected(id);
+      }
+   }
+
+   return (
+      <div className={ "square" } onClick={ handleFieldClick }>
+         <span>{ fieldMark }</span>
+      </div>
+   );
+};
