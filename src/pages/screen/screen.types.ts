30fac2e306e9a8a56636a005a3e8dbcdfd5e2531
LauraEsmoris <lesmoris0web@gmail.com>
2023-07-02 16:47:04 -0500

    stats

--- src/pages/screen/screen.types.ts
+++ src/pages/screen/screen.types.ts
@@ -1,3 +1,12 @@
 export  type tScreen = "playForm" | "feed" | "game";
 
-
+export interface IScreen {
+   fun:number,
+   age:number,
+   heart:number,
+   hungry:number,
+   name:string,
+   dinosaur:string,
+   color:string,
+   isAlive:boolean
+}
